c30df0e2417ce3762b8662b1529183a38ed26f1e
rodrigo.silva.cunha <rodrigo.silva.cunha@g.globo>
2022-07-17 12:56:23 -0300

    plus

--- app/infracode/hello.sh
+++ app/infracode/hello.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-hosts=$(cat ../env/hosts)
+hosts=$(cat ../env/hosts |egrep '[0-9]'|awk '{print $1}')
 
 function foo(){
 for x in $hosts;
 do
-echo $x
+ssh -o "StrictHostKeyChecking=no"  -o "ConnectTimeout=3" root@$x 'echo $HOSTNAME'
 done
 }
 
